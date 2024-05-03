8b05c09621d2906b5939637b8bbf8c476564f0bc
Junior Loureiro <gelson.junior_@hotmail.com>
2024-05-02 22:24:52 -0300

    Pessoa Contrutor e Setters

--- allscout/src/main/java/com/example/senac/Model/Pessoa.java
+++ allscout/src/main/java/com/example/senac/Model/Pessoa.java
@@ -1,5 +1,16 @@
 package com.example.senac.Model;
 
 public class Pessoa {
-
+    private String nome;
+    //Constructor
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+    //Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
