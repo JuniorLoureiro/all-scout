ebfb6ce07ab250f6a5d733329a1071f4b82f41c5
Lucas Laner <lucaslanerfornari@gmail.com>
2024-05-16 00:21:04 -0300

    LoginView

--- allscout/src/main/java/com/example/senac/App.java
+++ allscout/src/main/java/com/example/senac/App.java
@@ -1,13 +1,23 @@
 package com.example.senac;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.example.senac.View.LoginView;
+
+import javax.swing.*;
+
+
+public class App extends JFrame {
+    public static void main(String[] args) {
+        //SwingUtilities.invokeLater(() -> {
+            App frame = new App();
+            frame.setTitle("AllScout");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().add(new LoginView());
+            frame.pack();
+            frame.setLocationRelativeTo(null); // Centralizar a janela
+            frame.setVisible(true);
+        };
     }
-}
+
+
+    
+
