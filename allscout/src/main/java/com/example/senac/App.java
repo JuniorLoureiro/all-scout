1bb84070e549e2ef8773dd65755b33eed916db81
Junior Loureiro <gelson.junior_@hotmail.com>
2024-06-08 01:35:58 -0300

    Conexão com o banco de dados

--- allscout/src/main/java/com/example/senac/App.java
+++ allscout/src/main/java/com/example/senac/App.java
@@ -1,12 +1,28 @@
 package com.example.senac;
 
+import com.example.senac.Model.Atletas;
+import com.example.senac.Model.Pessoa;
 import com.example.senac.View.LoginView;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.swing.*;
 
 
 public class App extends JFrame {
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+        // Inserindo uma nova pessoa no banco de dados
+        entityManager.getTransaction().begin();
+        Pessoa p1 = new Pessoa("Pedro");
+        entityManager.persist(p1);
+        entityManager.getTransaction().commit();
+        // Fechando o EntityManager e o EntityManagerFactory
+        entityManager.close();
+        entityManagerFactory.close();
         //SwingUtilities.invokeLater(() -> {
             App frame = new App();
             frame.setTitle("AllScout");
