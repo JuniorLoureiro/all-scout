ba84fbd83f9132c8c9ce54ab0df783b0abf110fe
Junior <gelson.junior_@hotmail.com>
2024-07-04 09:52:00 -0300

    testes

---
+++ allscout/src/test/java/atleta/salvar/AtletaException.java
@@ -0,0 +1,5 @@
+package atleta.salvar;
+
+public class AtletaException {
+
+}
--- allscout/src/test/java/com/example/senac/AppTest.java
+++ allscout/src/test/java/com/example/senac/AppTest.java
@@ -1,20 +1,29 @@
 package com.example.senac;
 
-import static org.junit.Assert.assertTrue;
+import com.example.senac.Model.Atletas;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+import atleta.salvar.AtletaException;
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue(true);
+    }
+
+    @Test
+    public void testaAGravacaoDeUmAtletaJaCadastrado() {
+        Atletas atleta = new Atletas("LÉO JARDIM", "1995-03-20", "VASCO DA GAMA", "GOLEIRO", "DIREITA", "BRASIL", 1, 188);
+        assertThrows(atleta.salvar.AtletaException.class);
+    }
+
+    private void assertThrows(Class<AtletaException> class1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'assertThrows'");
     }
 }
